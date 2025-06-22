da71dc4db52b9dba4b7bf7c63205a13c1db84783
1Shinzou <168485081+1Shinzou@users.noreply.github.com>
2025-06-22 10:26:44 +0200

    HMM bombing and aa defines copied

--- common/defines/msb_defines.lua
+++ common/defines/msb_defines.lua
@@ -253,8 +253,8 @@ NDefines.NAir.COMBAT_MULTIPLANE_CAP = 3							-- How many planes can shoot at ea
 NDefines.NAir.COMBAT_DAMAGE_SCALE = 0.7						-- Higher value = more shot down planes
 -- Bombing Fix
 NDefines.NSupply.RAILWAY_FLOW_PENALTY_PER_DAMAGED = 4.9
-NDefines.NAir.AIR_WING_BOMB_DAMAGE_FACTOR = 0.5 --Chrom Bombing Fix; HMM = 0.6
-NDefines.NMilitary.ANTI_AIR_TARGETTING_TO_CHANCE = 0.05 -- Vanilla 0.07
+NDefines.NAir.AIR_WING_BOMB_DAMAGE_FACTOR = 0.45 --Chrom Bombing Fix; HMM = 0.6
+NDefines.NMilitary.ANTI_AIR_TARGETTING_TO_CHANCE = 0.09 -- Vanilla 0.07
 NDefines.NAir.AA_INDUSTRY_AIR_DAMAGE_FACTOR = 0 --Vanilla -0.12
  ---AS KAR MOD --- FUCK FLICKERING ----
  NDefines.NAir.EFFICIENCY_REGION_CHANGE_DAILY_GAIN_DEFAULT = 0.25	-- Default how much efficiency to regain per day. Gain applied hourly.

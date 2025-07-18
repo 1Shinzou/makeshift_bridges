3329fef6ad194a08a5938c06d72c001eea91d68c
1Shinzou <168485081+1Shinzou@users.noreply.github.com>
2025-07-18 17:36:08 +0200

    Less convoys needed for long range lend leases

--- common/defines/msb_defines.lua
+++ common/defines/msb_defines.lua
@@ -217,6 +217,7 @@ NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 7
 NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_RAILWAY_DAMAGE_SPILL_FACTOR = 0 -- Portion of train damage to additionally deal to railways
 
 NDefines.NCountry.FUEL_LEASE_CONVOY_RATIO = 0.0001 -- Fuel takes less convoys
+NDefines.NCountry.CONVOY_LENDLEASE_RANGE_FACTOR = 0.01 -- How much range affects convoy need for lend lease
 
 
 

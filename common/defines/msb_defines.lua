58fde75353b2652b8d23dbe3feeacfb1281858c8
1Shinzou <168485081+1Shinzou@users.noreply.github.com>
2025-12-06 11:50:15 +0100

    Old vanilla cas defines

--- common/defines/msb_defines.lua
+++ common/defines/msb_defines.lua
@@ -236,6 +236,8 @@ NDefines.NGeography.MEDITERRANEAN_SEA_REGIONS = { 15, 29, 68, 69, 168, 169, 202,
 
 -- Air Rework
 NDefines.NMilitary.LAND_AIR_COMBAT_MAX_PLANES_PER_ENEMY_WIDTH = 1   --vanilla 3
+NDefines.NAir.LAND_AIR_COMBAT_STR_DAMAGE_MODIFIER = 0.032 -- vanilla 0.08
+NDefines.NAir.LAND_AIR_COMBAT_ORG_DAMAGE_MODIFIER = 0.032 -- vanilla 0.10
 NDefines.NAir.DISRUPTION_FACTOR = 5.5 --cas enters battle at 30+%
 NDefines.NAir.AIR_MORE_GROUND_CREWS_COST = 0.0
 NDefines.NAir.COMBAT_MULTIPLANE_CAP = 3							-- How many planes can shoot at each plane on other side ( if there are 100 planes we are atttacking COMBAT_MULTIPLANE_CAP * 100 of our planes can shoot )
